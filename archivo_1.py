39cd5ced4d96c48d3846998fb9234f9e50ba8990
Enzo Gonzales <enzo.gonzales98@gmail.com>
2024-03-26 22:53:57 -0300

    4° commit

--- archivo_1.py
+++ archivo_1.py
@@ -1,5 +1,7 @@
-# ESTO ES EL PRIMER TEXTO (1° commit)
+# ESTO ES EL PRIMER TEXTO (1°commit)
 
 # ESTO ES EL SEGUNDO TEXTO (2°commit)
 
-# ESTO ES EL TERCER TEXTO (3° commit)
+# ESTO ES EL TERCER TEXTO (3°commit)
+
+# ESTO ES EL CUARTO TEXTO (4°commit)
